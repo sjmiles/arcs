71aa4f290862470a906fbcde42d4d7bb5a45a4b0
Alex Rosengarten <alxrsngrtn@google.com>
2019-05-09 16:30:32 -0700

    added minimial types to services.ts. Froze services.

--- src/runtime/services.ts
+++ src/runtime/services.ts
@@ -7,12 +7,34 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
+// TODO(alxr): should these go into `functional`?
+interface Dictionary<T>  {
+  [key: string]: T;
+}
+
+type Mapper<I, O> = (input: I) => O;
+
+
+interface Service {
+  // tslint:disable-next-line:no-any
+  [name: string]: Mapper<ServiceRequest, Promise<any>>;
+}
+
+type Registry = Dictionary<Service>;
+
+interface ServiceRequest {
+  service: string;
+  invoke: string;
+  call?: string;
+}
+
 export class Services {
-  static registry: {};
-  static register(name, service: {}) {
+  static registry: Registry = {};
+
+  static register(name: string, service: Service): void {
     Services.registry[name] = service;
   }
-  static async request(request) {
+  static async request(request: ServiceRequest) {
     let {service: name, invoke, call} = request;
     if (call) {
       [name, invoke] = call.split('.');
@@ -27,10 +49,10 @@ export class Services {
   }
 }
 
-Services.registry = {};
+Object.freeze(Services);
 
 Services.register('test', {
-  async classify(request) {
+  async classify(request: ServiceRequest) {
     return {data: `it's a pig, that don't fly straight`};
   }
 });
